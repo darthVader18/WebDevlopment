4142d7fb14d5640eef67bb621fc50c2a3ff8f5dc
Chaitanya <50162503+darthVader18@users.noreply.github.com>
2021-10-13 17:46:20 +0530

    13-10-2021

---
+++ Training/API/api.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  $("#submit").click(function () {
+    // debugger;
+    var id = $("#id").val();
+    var name = $("#name").val();
+    var description = $("#description").val();
+
+    // $.ajax({
+    //     url: "https://localhost:44376/Employee",
+    //     method: "GET",
+
+    //     success: function (data) {
+    //         alert('Updated Successfully');
+    //     },
+    //     error: function (msg) { alert("Error"); }
+    // });
+    $.ajax({
+      url: "https://localhost:44376/Employee",
+      method: "POST",
+      dataType: "json",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: JSON.stringify({
+        id: id,
+        name: name,
+        description: description,
+      }),
+      success: function (data, status, xhr) {
+        // success callback function
+        $("p").append(
+          "Name: " + data.name + " " + "ID: "+ data.id + " " + "Department: " + data.desc
+        );
+      },
+      error: function (request, status, error) {
+        alert("Error: " + request.responseText);
+      },
+    });
+  });
+});
